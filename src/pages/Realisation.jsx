0379c5f1a800dbf18f51d2ee00f505157a484477
Loane <loane.sene@etudiant.univ-lr.fr>
2023-12-21 15:20:34 +0100

    mots clefs realisation

--- src/pages/Realisation.jsx
+++ src/pages/Realisation.jsx
@@ -21,8 +21,8 @@ const Realisation = () => {
                     <img className="realisations__img" src={imagerealisation2} alt="projet1" />
                     <figcaption class="realisations__libelle">
                         <h3 className="realisations__name">Soutien à l'apprentissage de la lecture pour les détenus en situation d'illettrisme</h3>
-                        <p className="realisations__content">Nous avons eu la chance d'initier notre entreprise avec un projet phare : une application web qui vise à offrir une meilleure approche d'apprentissage du français aux illettrés, aux allophones et aux analphabètes, dans le but de favoriser leur réinsertion future.</p><br></br>
-                        <p className="realisations__content">Étant profondément touchées par leur situation, nous avons décidé de nous porter volontaires, offrant nos services gratuitement pour les aider à améliorer leur situation et à bénéficier d'un meilleur système d'éducation. Notre objectif est de contribuer à leur réinsertion en proposant des outils éducatifs adaptés à leurs besoins spécifiques.</p>
+                        <p className="realisations__content">Nous avons eu la chance d'initier notre entreprise avec un <strong className="strong2">projet phare</strong> : une <strong className="strong2">application web </strong> qui vise à offrir une <strong className="strong2">meilleure approche d'apprentissage</strong> du français aux illettrés, aux allophones et aux analphabètes, dans le but de favoriser leur réinsertion future.</p><br></br>
+                        <p className="realisations__content">Étant profondément touchées par leur situation, nous avons décidé de nous porter volontaires, offrant nos services gratuitement pour les aider à améliorer leur situation et à bénéficier d'un meilleur système d'éducation. <strong className="strong2">Notre objectif</strong> est de contribuer à leur réinsertion en proposant des outils éducatifs adaptés à leurs <strong className="strong2">besoins spécifiques.</strong></p>
                     </figcaption>
                 </figure>
 
