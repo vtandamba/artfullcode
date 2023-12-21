a17bbbd888b679bc943ecfec53777204fa6f64fe
Loane <loane.sene@etudiant.univ-lr.fr>
2023-12-21 10:44:55 +0100

    changement image

--- src/pages/Realisation.jsx
+++ src/pages/Realisation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../components/Nav';
 import projet1 from "../assets/projet1.png"
 import projet1Logo from "../assets/projet1Logo.png"
-import imagerealisation from "../assets/services/image-realisation.jpg"
+import imagerealisation2 from "../assets/services/image-realisation2.jpg"
 import Nav from "../components/Nav";
 const Realisation = () => {
 
@@ -18,7 +18,7 @@ const Realisation = () => {
                 </figure> */}
 
                 <figure className="realisations__projet">
-                    <img className="realisations__img" src={imagerealisation} alt="projet1" />
+                    <img className="realisations__img" src={imagerealisation2} alt="projet1" />
                     <figcaption class="realisations__libelle">
                         <h3 className="realisations__name">Soutien à l'apprentissage de la lecture pour les détenus en situation d'illettrisme</h3>
                         <p className="realisations__content">Nous avons eu la chance d'initier notre entreprise avec un projet phare : une application web qui vise à offrir une meilleure approche d'apprentissage du français aux illettrés, aux allophones et aux analphabètes, dans le but de favoriser leur réinsertion future.</p><br></br>
