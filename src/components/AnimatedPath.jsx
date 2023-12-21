5eae92f737284e67c87036c03f3d9c629d6f12b4
tandambav <victoriatandam@gmail.com>
2023-12-21 21:10:22 +0100

    envois des avis

---
+++ src/components/AnimatedPath.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const AnimatedPath = props => {
+  const pathRef = useRef();
+  const {
+    start,
+    end
+
+  } = props;
+
+  useEffect(() => {
+    const myPath = pathRef.current;
+    const pathLength = myPath.getTotalLength();
+    myPath.style.strokeDasharray = pathLength;
+    myPath.style.strokeDashoffset = pathLength;
+
+    gsap.to(pathRef.current, {
+      strokeDashoffset: 0,
+      ease: 'linear',
+      scrollTrigger: {
+        trigger: ".leSvg",
+        start: "top 75%",
+        end: "bottom 65%",
+        // start: { start },
+        // end: { end },
+        toggleActions: "restart pause reverse pause",
+        markers: false,
+        scrub: 3,
+      },
+      duration: 4,
+    });
+  }, []);
+
+  return (
+    <div className="leSvg">
+      <svg width="1440" height="420" viewBox="0 0 1440 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          ref={pathRef}
+          id="myPath"
+          stroke="#27496D"
+          d="M0 .5C1229.72 103.387 1437.22 202.809 1439.5 412M0 139.5C746.135 90.25 1089.23 101.226 1439.5 255"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default AnimatedPath;
---
+++ src/components/AnimatedPath_haut.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const AnimatedPath = () => {
+  const pathRef = useRef();
+
+  useEffect(() => {
+    const myPath = pathRef.current;
+    const pathLength = myPath.getTotalLength();
+    myPath.style.strokeDasharray = pathLength;
+    myPath.style.strokeDashoffset = pathLength;
+
+    gsap.to(pathRef.current, {
+      strokeDashoffset: 0,
+      ease: 'linear',
+      scrollTrigger: {
+        trigger: ".leSvg",
+        start: "top 100%",
+        end: "bottom 0%",
+        toggleActions: "restart pause reverse pause",
+        markers: true,
+        scrub: 3,
+      },
+      duration: 4,
+    });
+  }, []);
+
+  return (
+    <div className="leSvg leSvg__haut">
+      <svg width="1440" height="420" viewBox="0 0 1440 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          ref={pathRef}
+          id="myPath"
+          d="M0 0C0 349.981 957.333 433.806 1436 431.971" stroke="#053F5E" />
+      </svg>
+    </div>
+  );
+};
+
+export default AnimatedPath;
