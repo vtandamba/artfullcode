7b9b264d0afae2ba43425fae7fe406d0258afb4b
tandambav <victoriatandam@gmail.com>
2024-02-25 11:28:12 +0100

    notre equipe 1

--- src/components/Equipe.jsx
+++ src/components/Equipe.jsx
@@ -6,76 +6,76 @@ import loaneImg from '../assets/membres/loanephoto2.png';
 import deliaImg from '../assets/membres/deliaphoto2.png';
 import clarenceImg from '../assets/membres/clarencephoto2.png'
 
-const Equipe  = () => {
-    return <section class="equipe">
+const Equipe = () => {
+  return <section class="equipe">
     <h2 className="equipe__title">
-     Rencontrez nos membres
+      Rencontrez nos membres
     </h2>
 
     <article className="equipe__membres">
       <figure className="equipe__fig equipe--bleu">
-        <img src={cole} alt="victoria" className="equipe__img"/>
+        <img src={victoriaImg} alt="victoria" className="equipe__img" />
         <figcaption class="membre">
           <div className="membre__infos">
-              <p className="membre__nom">Victoria Tandamba </p><br></br>
-              <p className="membre__filiere">Développeuse Full Stack</p>
+            <p className="membre__nom">Victoria Tandamba </p><br></br>
+            <p className="membre__filiere">Développeuse Full Stack</p>
           </div>
-          <a href="https://www.linkedin.com/in/victoria-tandamba/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin"/></a>
-   
+          <a href="https://www.linkedin.com/in/victoria-tandamba/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin" /></a>
+
         </figcaption>
       </figure>
 
-        
-        <figure className="equipe__fig equipe--bleu">
-        <img src={cole} alt="" className="equipe__img"/>
+
+      <figure className="equipe__fig equipe--bleu">
+        <img src={deliaImg} alt="" className="equipe__img" />
         <figcaption className="membre">
           <div className="membre__infos">
-            <p className="membre__nom">Marilyne Delia Tsene</p><br></br>
+            <p className="membre__nom">Marilyne Tsene</p><br></br>
             <p className="membre__filiere">Développeuse Full Stack</p>
           </div>
-          <a href="www.linkedin.com/in/delia-marilyne-tsene-4895a52a9" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin"/></a>
-   
+          <a href="www.linkedin.com/in/delia-marilyne-tsene-4895a52a9" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin" /></a>
+
         </figcaption>
-        </figure>
+      </figure>
 
       <figure className="equipe__fig equipe--none">
-      <img src={cole} alt="" className="equipe__img"/>
+        <img src={loaneImg} alt="" className="equipe__img" />
         <figcaption className="membre">
           <div className="membre__infos">
             <p className="membre__nom">Loane Sene</p><br></br>
             <p className="membre__filiere">Web Designeuse </p>
           </div>
-          <a href="https://www.linkedin.com/in/loane-s%C3%A9n%C3%A9/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin"/></a>
-   
+          <a href="https://www.linkedin.com/in/loane-s%C3%A9n%C3%A9/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin" /></a>
+
         </figcaption>
       </figure>
 
       <figure className="equipe__fig equipe--none">
-      <img src={cole} alt="" className="equipe__img"/>
+        <img src={clarenceImg} alt="" className="equipe__img" />
         <figcaption className="membre">
           <div className="membre__infos">
             <p className="membre__nom">Clarence Noirot</p><br></br>
             <p className="membre__filiere">Web Designeuse</p>
           </div>
-          <a href="https://www.linkedin.com/in/clarence-noirot-5b517a251/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin"/></a>
-   
+          <a href="https://www.linkedin.com/in/clarence-noirot-5b517a251/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" className="membre__linkedin" /></a>
+
         </figcaption>
       </figure>
     </article>
 
     <svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">
 
-  <path d="M 50 50 Q 150 0, 250 50" stroke="turquoise" fill="none"/>
-  
+      <path d="M 50 50 Q 150 0, 250 50" stroke="turquoise" fill="none" />
 
-  <circle cx="50" cy="50" r="30" fill="white" stroke="black"/>
-  
 
-  <text x="50" y="55" font-family="Satoshi, sans-serif" font-size="18" text-anchor="middle">1</text>
-  
+      <circle cx="50" cy="50" r="30" fill="white" stroke="black" />
 
-  <text x="50" y="80" font-family="Satoshi, sans-serif" font-size="18" text-anchor="middle">16%</text>
-</svg>
+
+      <text x="50" y="55" font-family="Satoshi, sans-serif" font-size="18" text-anchor="middle">1</text>
+
+
+      <text x="50" y="80" font-family="Satoshi, sans-serif" font-size="18" text-anchor="middle">16%</text>
+    </svg>
 
   </section>
 }
